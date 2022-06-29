cf56397a53a4d9c1fc9baae5b8e51d11e87f351f
6jodeci <egor0263s@gmail.com>
2022-06-29 16:57:09 +0500

    add templates

--- pkg/main.go
+++ pkg/main.go
@@ -4,6 +4,7 @@ import (
 	"examplechat/controllers"
 	"examplechat/database"
 	"examplechat/middlewares"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,11 +19,24 @@ func main() {
 }
 
 func initRouter() *gin.Engine {
+
 	router := gin.Default()
+	router.LoadHTMLGlob("../templates/html/*.html")
+	router.Static("/css", "../templates/css/")
+	router.GET("/", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "chat_homepage.html", gin.H{"title": "homepage"})
+	})
+	router.GET("/chat/registration", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "registration.html", gin.H{"title": "registration"})
+	})
+	router.GET("/chat/authorization", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "authorization.html", gin.H{"title": "authorization"})
+	})
 	api := router.Group("/api")
 	{
 		api.POST("/token", controllers.GenerateToken)
 		api.POST("/user/register", controllers.RegisterUser)
+
 		secured := api.Group("/secured").Use(middlewares.Auth())
 		{
 			secured.GET("/ping", controllers.Ping)
